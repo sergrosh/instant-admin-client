a49bc7f26a0a21886b2bfb3999a783f4db1da0a7
Teodor Rupi <teodorrupi@gmail.com>
2016-02-27 13:24:38 +0100

    initial commit of instant client with angluar via yeoman gemerator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name instantAdministrationClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the instantAdministrationClientApp
+ */
+angular.module('instantAdministrationClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name instantAdministrationClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the instantAdministrationClientApp
+ */
+angular.module('instantAdministrationClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
