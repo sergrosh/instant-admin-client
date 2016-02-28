5dbb02695506b6cc3290cd2b8cd47d6e731c42ff
Teodor Rupi <teodorrupi@gmail.com>
2016-02-29 00:45:45 +0100

    adding multiselect derivative

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,8 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-bootstrap-multiselect/angular-bootstrap-multiselect.js',
+      'bower_components/lodash/dist/lodash.compat.js',
+      'bower_components/angularjs-dropdown-multiselect/src/angularjs-dropdown-multiselect.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
